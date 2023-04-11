2d08f5ad7ed01e0c8fcd0d2c2405df17143817b3
Shishira <shishiraiyar@gmail.com>
2023-04-11 18:12:09 +0530

    svg images

--- static/js/app.js
+++ static/js/app.js
@@ -7,29 +7,22 @@ async function displayGrid(){
     let grid = data["grid"]
     let htmlString = ""
     for (let i=0; i<grid.length; i++){
-        if (grid[i]["value"] == 0)
-            htmlString += '<div name="square" class="square fullImg" id="'+ i +'"><img /></div>'
-        else{
-            // console.log(grid[i]["value"], grid[i]["colour"])
-            let imgFile = getImageFile(grid[i]["value"], grid[i]["colour"]) 
-            // console.log(imgFile)
-            htmlString += '<div name="square" class="square fullImg" id="' + i + '"><img src="' + imgFile + '"/></div>'
-        }
+        let svgImg = getSvgImg(grid[i]["value"], grid[i]["colour"])  // <svg> </svg>
+        htmlString +=  svgImg 
+        if (i%6 == 5) htmlString+="<br>"
+
     }
     document.getElementsByClassName("grid")[0].innerHTML = htmlString;
 
     for (let i=0; i<grid.length; i++){
-        let squareElement = document.getElementsByName("square")[i]
+        let squareElement = document.getElementsByClassName("svgImg")[i]
         squareElement.addEventListener("click", function(){
-            onClick(squareElement.id)
+            onClick(i)
         })
     } 
 }
 
 async function onClick(squareNumber){
-    // playerId = document.getElementById("player").value
-    console.log(playerId)
-    console.log("CLOCKCKC")
     let data = {"playerId": playerId, "square": squareNumber}
     await fetch("/move/" + roomId, {
         method:"POST",
@@ -40,58 +33,25 @@ async function onClick(squareNumber){
         body: JSON.stringify(data)
     })
 }
+
 window.onload = async (event) => {
     await displayGrid()
+    enableStartButton()
     setInterval(updateGrid, 500)
   };
 
 
 async function updateGrid(){
-    console.log("A")
     let data = await getData()
     let grid = data["grid"]
 
     for (let i=0; i<grid.length; i++){
-        let squareElement = document.getElementsByName("square")[i]
-        if (grid[i]["value"] != 0){
-            let imgFile = getImageFile(grid[i]["value"], grid[i]["colour"]) 
-            squareElement.childNodes[0].src = imgFile
-        }
-        else 
-            squareElement.childNodes[0].src = "../static/images/blank.png"
+        let svg = document.getElementsByClassName("svgImg")[i]
+        svg.childNodes[0].setAttribute("d", getSvgPath(grid[i]["value"]))
+        svg.childNodes[0].setAttribute("fill", getColourHex(grid[i]["colour"]))
+
     }
 }
-
-// async function post(data){//sends stuff to server. write one more func that gets data and calls this
-//     let response = await fetch("/api", {
-//         method:"POST",
-//         headers: {
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify(data)
-//     }).then(response =>response.json())
-//     console.log(response)
-
-// }
-
-
-
-document.getElementById("start").addEventListener("click", async()=>{
-    await fetch("/start/" + roomId, {
-        method:"POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({"id": playerId})
-    })
-    //delets the button here
-})
-
-
-
-
 
 
 
@@ -118,5 +78,60 @@ function getImageFile(value, colour){
     return imgFile
 }
 
+function getSvgImg(value, colour){ 
+    
+    let colourHex = getColourHex(colour)
+    let path = getSvgPath(value)
+
+    let svg = `<svg viewBox="0 0 100 100" class="svgImg"><path d="${path}"
+    fill="${colourHex}" stroke="#000000" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`
+
+    return svg
+}
+
+function getSvgPath(value){
+    value = value%5             //for future if needed
+    switch(value){
+        case 0: return "";
+
+        case 1: return "M 50 30 a 20 20 0 1 1 0 40 a 20 20 0 1 1 0 -40";
+
+        case 2: return "M 50 35 a 20 20 0 1 1 0 30 a 20 20 0 1 1 0 -30" ; 
+
+        case 3: return "M 50 65 a 15 15 0 1 0 13 -22.5 a 15 15 0 1 0 -26 0 a 15 15 0 1 0 13 22.5"; 
+
+        case 4: return "M 50 30 a 15 15 0 1 1 20 20 a 15 15 0 1 1 -20 20 a 15 15 0 1 1 -20 -20 a 15 15 0 1 1 20 -20"; 
+    }
+}
+
+function getColourHex(colour){
+    const colours = ["#ff0000", "#00ff00", "#0000ff", "#00ffff", "#ff00ff", "#ffff00"]
+    return colours[colour%colours.length]
+}
+
+function enableStartButton(){
+    let startButton = document.getElementById("start")
+    if (!startButton)
+        return
+    startButton.addEventListener("click", async()=>{
+        await fetch("/start/" + roomId, {
+            method:"POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"id": playerId})
+        })
+        startButton.remove()
+    })
+    
+}
 
 
+        // if (grid[i]["value"] == 0)
+        //     htmlString += '<div name="square" class="square fullImg" id="'+ i +'"><img /></div>'
+        // else{
+        //     let imgFile = getImageFile(grid[i]["value"], grid[i]["colour"]) 
+        //     htmlString += '<div name="square" class="square fullImg" id="' + i + '"><img src="' + imgFile + '"/></div>'
+        // }
